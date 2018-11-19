2d7bf608d07b9ebdc49315498188084751b665dc
Arek Jenerowicz <szejos@gmail.com>
2018-11-19 09:12:00 +0100

    firstlast.c add

---
+++ Part_2/firstlast.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int (int* tabInput, int* days)
+{
+
+
+    return returnNum;
+}
+
+void test_cases()
+{
+
+}
+
+int main()
+{
+
+    int tabInput[2];
+    scanf()
+
+    test_cases();
+
+}
+
